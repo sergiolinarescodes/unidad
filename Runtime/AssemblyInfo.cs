f37fb9c7d8d3e74b522c593ebd033907a0ed9dd8
sergiolinarescodes <sergiolinarescodes@users.noreply.github.com>
2026-03-02 11:35:16 +0100

    Add ScenarioHelpers utility and InternalsVisibleTo for game assemblies
    - Add ScenarioHelpers.SetColor with cached shader reference
    - Add InternalsVisibleTo for Experimental.Core and Experimental.Core.Tests

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Unidad.Core.Editor")]
 [assembly: InternalsVisibleTo("Unidad.Scripting.Runtime")]
 [assembly: InternalsVisibleTo("Unidad.Scripting.Tests")]
+[assembly: InternalsVisibleTo("Experimental.Core")]
+[assembly: InternalsVisibleTo("Experimental.Core.Tests")]
